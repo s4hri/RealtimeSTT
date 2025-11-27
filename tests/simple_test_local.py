a8afb8f78896bced5b791bd5223c74c68d2fcbf9
Gioele Migno <migno.gioele@gmail.com>
2025-11-27 16:29:01 +0100

    added func for manual download whisper model

--- tests/simple_test_local.py
+++ tests/simple_test_local.py
@@ -3,6 +3,7 @@ import torch
 import sys
 import os 
 
+import huggingface_hub
 from RealtimeSTT import AudioToTextRecorder
 
 def download_silero_vad(dir_output):
@@ -22,27 +23,60 @@ def download_silero_vad(dir_output):
     os.system(cmd)
     return dir_silero_vad
 
+def download_whisper(dir_output, model_name_hf):
+    model_name = model_name_hf.replace("/", "___")
+
+    dir_model = os.path.join(dir_output, 'whisper_models', model_name)
+    if os.path.exists(dir_model):
+        print(f"Whisper model already downloaded in {dir_model}")
+        return dir_model
+    
+    print(f"Downloading whisper model in {dir_model}")
+    os.makedirs(dir_model)
+
+    #repo_id = "Systran/faster-whisper-tiny"
+    repo_id = model_name_hf
+    revision = None
+    local_files_only = False
+    output_dir = dir_model
+    os.makedirs(output_dir, exist_ok=True)
+
+    kwargs = {
+            "local_files_only": local_files_only,
+            "revision": revision
+    }
+
+    if output_dir is not None:
+            kwargs["local_dir"] = output_dir
+
+    # if cache_dir is not None:
+    #         kwargs["cache_dir"] = cache_dir
+
+    # if use_auth_token is not None:
+    #         kwargs["token"] = use_auth_token
+
+    return huggingface_hub.snapshot_download(repo_id, **kwargs)
+
 FILEPATH_ABS_SCRIPT = os.path.realpath(__file__)
 DIR_ABS_SCRIPT = os.path.dirname(FILEPATH_ABS_SCRIPT)
 
 DIR_CACHE = "/workdir/assets"
 DIR_CACHE_WHISPER = os.path.join(DIR_CACHE, "whisper_models")
-DIR_CACHE_SILERO_VAD = download_silero_vad(DIR_CACHE)
 
-import sys
+DIR_CACHE_SILERO_VAD = download_silero_vad(DIR_CACHE)
 sys.path.append(DIR_CACHE_SILERO_VAD)
 from hubconf import silero_vad
 
 
 if __name__ == '__main__':
+    dir_model = download_whisper(DIR_CACHE, "Systran/faster-whisper-tiny")
 
     print("Wait until it says 'speak now'")
-    recorder = AudioToTextRecorder(model='tiny', #'large-v3', #'tiny',
+    recorder = AudioToTextRecorder(model=dir_model,
                                     debug_mode=True, 
                                     silero_sensitivity=0.01,
                                     language='en',
-                                    silero_load_func=silero_vad,
-                                    download_root=DIR_CACHE_WHISPER)
+                                    silero_load_func=silero_vad)
     while True:
         '''
         timeout_wait_start: Timeout during waiting for speech start
