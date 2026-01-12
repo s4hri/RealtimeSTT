0ee08cf3773a6464a2db5e8bf86423206af54df6
Gioele Migno <migno.gioele@gmail.com>
2026-01-12 08:45:08 +0100

    Added customization args in simple_test_local.py

--- tests/simple_test_local.py
+++ tests/simple_test_local.py
@@ -68,26 +68,76 @@ DIR_CACHE_SILERO_VAD = download_silero_vad(DIR_CACHE)
 sys.path.append(DIR_CACHE_SILERO_VAD)
 from hubconf import silero_vad
 
-def my_start_callback():
+def on_recording_start():
     print("\nRecording started!")
 
-def my_stop_callback():
+def on_recording_stop():
     print("\nRecording stopped!")
 
+def on_vad_start():
+    print("\nvad start")
+
+def on_vad_stop():
+    print("\nvad stop")
+
+
+'''
+        - silero_sensitivity (float, default=SILERO_SENSITIVITY): Sensitivity
+            for the Silero Voice Activity Detection model ranging from 0
+            (least sensitive) to 1 (most sensitive). Default is 0.5.
+        - silero_use_onnx (bool, default=False): Enables usage of the
+            pre-trained model from Silero in the ONNX (Open Neural Network
+            Exchange) format instead of the PyTorch format. This is
+            recommended for faster performance.
+        - silero_deactivity_detection (bool, default=False): Enables the Silero
+            model for end-of-speech detection. More robust against background
+            noise. Utilizes additional GPU resources but improves accuracy in
+            noisy environments. When False, uses the default WebRTC VAD,
+            which is more sensitive but may continue recording longer due
+            to background sounds.
+        - webrtc_sensitivity (int, default=WEBRTC_SENSITIVITY): Sensitivity
+            for the WebRTC Voice Activity Detection engine ranging from 0
+            (least aggressive / most sensitive) to 3 (most aggressive,
+            least sensitive). Default is 3.
+        - post_speech_silence_duration (float, default=0.6): Duration in
+            seconds of silence that must follow speech before the recording
+            is considered to be completed. This ensures that any brief
+            pauses during speech don't prematurely end the recording.
+        - ensure_sentence_starting_uppercase (bool, default=True): Ensures
+            that every sentence detected by the algorithm starts with an
+            uppercase letter.
+        - ensure_sentence_ends_with_period (bool, default=True): Ensures that
+            every sentence that doesn't end with punctuation such as "?", "!"
+            ends with a period
+'''
+
+
+config = {
+    "silero_sensitivity": 0.1,
+    "webrtc_sensitivity": 3,
+    "silero_use_onnx": True,
+    "silero_deactivity_detection": True,
+    "post_speech_silence_duration": 0.6
+}
 if __name__ == '__main__':
     dir_model = download_whisper(DIR_CACHE_WHISPER, "Systran/faster-whisper-tiny")
-    #dir_model = download_whisper(DIR_CACHE_WHISPER, "Systran/faster-whisper-large-v3")
+    #dir_model = download_whisper(DIR_CACHE_WHISPER, "Systran___faster-whisper-large-v3")
 
+    kwargs = config
+    kwargs.update({
+        "model": dir_model,
+        "language": 'en',
+        "debug_mode": True,
+        "silero_load_func": silero_vad,
+        "on_vad_start": on_vad_start,
+        "on_vad_stop": on_vad_stop,
+        "on_recording_start": on_recording_start,
+        "on_recording_stop": on_recording_stop,
+        "allowed_recording": False  # it will allowed in text()
+    })
     print("Wait until it says 'speak now'")
-    recorder = AudioToTextRecorder(model=dir_model,
-                                    debug_mode=True, 
-                                    silero_sensitivity=0.01,
-                                    language='en',
-                                    silero_load_func=silero_vad,
-                                    on_recording_start=my_start_callback,
-                                    on_recording_stop=my_stop_callback,
-                                    allowed_recording=False # it will allowed in text()
-                                    )
+    recorder = AudioToTextRecorder(**kwargs)
+
     while True:
         '''
         timeout_wait_start: Timeout during waiting for speech start
@@ -99,6 +149,7 @@ if __name__ == '__main__':
             text will be cut
         '''
 
+        # to fix, after timeout, the system continue to listen
         print("Listening...")
         kwargs = {
             "timeout_wait_start": 30.0,
